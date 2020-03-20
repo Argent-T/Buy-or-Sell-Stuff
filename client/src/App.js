31d4abf7a06b50c84627deea8b8954e90d61087b
Sharon Yelverton <ladysharonyel@me.com>
2020-03-20 00:39:55 -0400

    Landing page update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Footer from './components/Footer';
+
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 import Profile from './pages/Profile';
@@ -12,7 +14,6 @@ import Login from './pages/Login';
 function App() {
   return (
     <Router>
-      <Navbar />
       <Route exact path="/" component = {Landing} />
       <Route exact path="/profile/:id" component = {Profile} />
       <Route exact path="/buy" component = {Buy} />
@@ -20,7 +21,9 @@ function App() {
       <Route exact path="/signup" component = {Signup} />
       <Route exact path="/login" component = {Login} />
       {/* include route with Id for specific item detail page */}
+      <Footer />
     </Router>
+    
   );
 }
 
