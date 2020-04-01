86ef0c3e75b8f3a3b4e9bb1d3140eddde3fdb650
Matt Dominick <mdominick300@gmail.com>
2020-04-01 19:56:20 -0400

    update redirect on signup

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Navbar from "../components/Navbar";
+import { Redirect } from 'react-router-dom'
 
 class SignUp extends Component {
 	constructor() {
@@ -14,7 +15,8 @@ class SignUp extends Component {
 			city: '',
 			state: '',
 			zip: '',
-			email: ''
+			email: '',
+			redirectTo: null
 
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
@@ -61,52 +63,12 @@ class SignUp extends Component {
 
 
 	render() {
+		if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        } else {
 		return (
 			<>
 				<Navbar></Navbar>
-				{/* <div className="SignupForm">
-					<h4>Sign up</h4>
-					<form className="form-horizontal">
-						<div className="form-group">
-							<div className="col-1 col-ml-auto">
-								<label className="form-label" htmlFor="username">Username</label>
-							</div>
-							<div className="col-3 col-mr-auto">
-								<input className="form-input"
-									type="text"
-									id="username"
-									name="username"
-									placeholder="Username"
-									value={this.state.username}
-									onChange={this.handleChange}
-								/>
-							</div>
-						</div>
-						<div className="form-group">
-							<div className="col-1 col-ml-auto">
-								<label className="form-label" htmlFor="password">Password: </label>
-							</div>
-							<div className="col-3 col-mr-auto">
-								<input className="form-input"
-									placeholder="password"
-									type="password"
-									name="password"
-									value={this.state.password}
-									onChange={this.handleChange}
-								/>
-							</div>
-						</div>
-						<div className="form-group ">
-							<div className="col-7"></div>
-							<button
-								className="btn btn-primary col-1 col-mr-auto"
-								onClick={this.handleSubmit}
-								type="submit"
-							>Sign up</button>
-						</div>
-					</form>
-				</div> */}
-
 
 				<div className="container signupContent">
 					<h1 className="content is-large">Sign Up</h1>
@@ -243,6 +205,7 @@ class SignUp extends Component {
 
 			</>
 		)
+		}
 	}
 }
 export default SignUp;
